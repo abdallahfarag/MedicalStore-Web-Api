132284a218eac3fa9d60ca3fadca57e4d590d5b5
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-06-20 22:42:05 +0200

    -Edit Date type in order

---
+++ ITIGraduationProject/MedicalStoreWebApi/Migrations/202006202039301_EditOrderModelColNames.cs
@@ -0,0 +1,18 @@
+﻿namespace MedicalStoreWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditOrderModelColNames : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Orders", "DateAdded", c => c.DateTime(nullable: false, storeType: "smalldatetime"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Orders", "DateAdded", c => c.DateTime(nullable: false, storeType: "date"));
+        }
+    }
+}
